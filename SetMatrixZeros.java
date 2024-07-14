3aea1f1a8ed12535e699b356f657d9b2e0114a1c
Komal Garlapati <saikomalgkvs@gmail.com>
2024-07-14 15:02:25 +0530

    Q1 Completed

---
+++ SetMatrixZeros.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        List<int[]> lst = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0) lst.add(new int[]{i,j});
+            }
+        }
+        helper(matrix, lst);
+    }
+    private void helper(int[][] matrix, List<int[]> lst){
+        for(int[] cord : lst){
+            int i=cord[0];
+            int j=cord[1];
+            for(int col=0; col < matrix[0].length; col++){
+                matrix[i][col] = 0;
+            }
+            for(int row=0; row < matrix.length; row++){
+                matrix[row][j] = 0;
+            }
+        }
+    }
+}
