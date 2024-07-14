c18436c95212925c86702a4b96dead90daa65460
Komal Garlapati <saikomalgkvs@gmail.com>
2024-07-14 21:04:37 +0530

    Q5 completed

---
+++ Arrays/FindTheDuplicateNumber.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n+1];
+
+        for(int i=0; i<n; i++){
+            arr[nums[i]]++;
+            if(arr[nums[i]] >1) return nums[i];
+        }
+
+        return -1;
+    }
+}
