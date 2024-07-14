0d7390e3d33ef70ae7e993a44337373f1fd91b1f
Komal Garlapati <saikomalgkvs@gmail.com>
2024-07-14 15:30:19 +0530

    Q2 completed

---
+++ MoveZeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums.length == 1) return;
+
+        int i=0;
+        int p=0;
+
+        while(i<nums.length){
+            if(nums[i] !=0){
+                if(i!=p){
+                    nums[p] = nums[i];
+                    nums[i] = 0;
+                }
+                p++;
+            }
+            i++;
+        }
+    }
+}
