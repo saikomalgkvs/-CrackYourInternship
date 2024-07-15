3bfdff295b3c8624bddab24878aa643487596657
Komal Garlapati <saikomalgkvs@gmail.com>
2024-07-15 11:10:48 +0530

    updated

---
+++ Strings/LongestCommonPrefix.java
@@ -0,0 +1,26 @@
+package Strings;
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int count = 0;
+        boolean breaked =false;
+        int min = Integer.MAX_VALUE;
+        for(String s : strs){
+            min = Math.min(min, s.length());
+        }
+        for(int j=0; j<min; j++){
+            for(int i=0; i<strs.length-1; i++){
+                if(strs[i].charAt(j) == strs[i+1].charAt(j)){
+                    continue;
+                }
+                else{
+                    breaked = true;
+                    break;
+                }
+            }
+            if(breaked) break;
+            count++;
+        }
+        return strs[0].substring(0,count);
+    }
+}
