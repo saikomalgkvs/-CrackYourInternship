60eab8905124c60e23d77a308b4db3a52529de5f
Komal Garlapati <saikomalgkvs@gmail.com>
2024-07-17 19:01:51 +0530

    3 completed

---
+++ Graph/BFSofGraph.java
@@ -0,0 +1,29 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFSofGraph {
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> bfs = new ArrayList<Integer>();
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        
+        boolean[] visited = new boolean[V];
+        visited[0] = true;
+        while(!q.isEmpty()){
+            int out = q.poll();
+            bfs.add(out);
+            
+            for(int n : adj.get(out)){
+                if(!visited[n]){
+                    q.add(n);
+                    visited[n] = true;
+                }
+            }
+        }
+        return bfs;
+    }
+}
---
+++ Graph/FloodFill.java
@@ -0,0 +1,29 @@
+package Graph;
+
+public class FloodFill {
+    public int[][] floodFill(int[][] mat, int i, int j, int color) {
+        boolean[][] vis = new boolean[mat.length][mat[0].length];
+
+        vis[i][j] = true;
+
+        dfs(vis, mat, i,j, i, j+1, color); // right
+        dfs(vis, mat, i,j, i, j-1, color); // left
+        dfs(vis, mat, i,j, i-1, j, color); // up
+        dfs(vis, mat, i,j, i+1, j, color); // down
+
+        mat[i][j] = color;
+        return mat;
+    }
+    public void dfs(boolean[][] vis, int[][] mat, int i, int j, int p, int q, int color){
+        if(p<0 || q<0 || p>=mat.length || q>=mat[0].length) return;
+        if(mat[i][j] != mat[p][q]) return;
+        if(vis[p][q]) return;
+
+        vis[p][q] = true;
+        dfs(vis, mat, p,q, p, q+1, color); // right
+        dfs(vis, mat, p,q, p, q-1, color); // left
+        dfs(vis, mat, p,q, p-1, q, color); // up
+        dfs(vis, mat, p,q, p+1, q, color); // down
+        mat[p][q] = color;
+    }
+}
